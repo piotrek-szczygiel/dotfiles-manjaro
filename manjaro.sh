c34cb498ffda0d9ca6ce6ee17e8112bc811e1c42
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-03-02 00:25:31 +0100

    update

--- manjaro.sh
+++ manjaro.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v yay)" ]; then
-    echo "Error: yay is not installed." >&2
-    exit 1
-fi
-
 if [ ! -f "/etc/arch-release" ]; then
     echo "Error: this script only works on Arch Linux." >&2
     exit 1
@@ -21,6 +16,8 @@ sudo sed -i '/Color/s/^#//' /etc/pacman.conf
 
 echo_yellow "Updating system..."
 sudo pacman -Syuq --noconfirm
+sudo pacman -Sq --noconfirm yay
 
 yay -Sq --noconfirm yadm
 yadm clone https://github.com/piotrek-szczygiel/dotfiles-manjaro
+
